27910e76012d19f2076d557994155ba87c48ca85
zerolethanh <zero.lethanh@gmail.com>
2021-11-15 22:12:12 +0700

    now on UTC

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import {customAlphabet} from 'nanoid';
 import moment from 'moment';
 
 const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-const now = () => moment().format('YYMMDDTHHmmss');
+const nowOnUTC = () => moment().utc().format('YYMMDDTHHmmss');
 //`${d.getFullYear()}${d.getMonth()}${d.getDate()}T${d.getHours()}${d.getMinutes()}${d.getSeconds()}`
 
-export const genId = ({prefix, size = 5, randomSet}) => {
-	const nnid = customAlphabet(randomSet || alphabet, size);
-	return `${prefix}_${now()}_${nnid()}`;
+const genId = (prefix = '', size = 4, randomSet = alphabet) => {
+	const nnid = customAlphabet(randomSet, size);
+	return `${prefix}${nowOnUTC()}_${nnid()}`;
 };
+
+export default genId;
