524aec84f430645e1ad130feea6a17cc54be9dcd
LE VAN THANH <zero.lethanh@gmail.com>
2022-03-01 23:39:58 +0700

    use luxon

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import {customAlphabet} from 'nanoid';
-import moment from 'moment';
+import {DateTime} from 'luxon';
 
 const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-const nowOnUTC = () => moment().utc().format('YYMMDDTHHmmss');
+const nowOnUTC = () => DateTime.now().toUTC().toFormat('yyMMdd\'T\'hhmmss');
 //`${d.getFullYear()}${d.getMonth()}${d.getDate()}T${d.getHours()}${d.getMinutes()}${d.getSeconds()}`
 
 const genId = (prefix = '', size = 4, randomSet = alphabet) => {
-	const nnid = customAlphabet(randomSet, size);
-	return `${prefix}${nowOnUTC()}_${nnid()}`;
+    const nnid = customAlphabet(randomSet, size);
+    return `${prefix}${nowOnUTC()}_${nnid()}`;
 };
 
 export default genId;
--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import genId from './index.js';
 const l = console.log;
 
 for (let i = 0; i < 1_000; ++i) {
-	const id = genId('user', 5, '0123456789');
+	const id = genId('user', 5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz');
 	l(id);
 }
 
