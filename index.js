51c546dcc7b0a213ee124aeeb52b25e0a6174609
LE VAN THANH <zero.lethanh@gmail.com>
2022-03-05 14:43:25 +0700

    no dependencies

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 import {customAlphabet} from 'nanoid';
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc.js';
 
-dayjs.extend(utc);
 const numberSet = '0123456789';
 const uppercaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowercaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
 const alphabet = `${numberSet}${uppercaseAlphabet}${lowercaseAlphabet}`;
 
-const nowOnUTC = () => dayjs.utc().format('YYMMDD\THHmmss');
-//`${d.getFullYear()}${d.getMonth()}${d.getDate()}T${d.getHours()}${d.getMinutes()}${d.getSeconds()}`
+const nowOnUTC = () => {
+    const date = new Date();
+    const now_utc = Date.UTC(
+        date.getUTCFullYear(),
+        date.getUTCMonth(),
+        date.getUTCDate(),
+        date.getUTCHours(),
+        date.getUTCMinutes(),
+        date.getUTCSeconds(),
+        date.getMilliseconds(),
+    );
+    const dateUtc = new Date(now_utc);
+    const Y = `${dateUtc.getFullYear()}`.slice(-2);
+    const M = addZeroPrefix(dateUtc.getMonth() + 1);
+    const D = addZeroPrefix(dateUtc.getDate());
+    const H = addZeroPrefix(dateUtc.getHours());
+    const m = addZeroPrefix(dateUtc.getMinutes());
+    const s = addZeroPrefix(dateUtc.getSeconds());
+    const ms = addZeroPrefix(dateUtc.getMilliseconds());
+    return `${Y + M + D}T${H}${m}${s}${ms}`;
+};
+
+const addZeroPrefix = num => +num > 10 ? `${num}` : `0${num}`;
 
 const genId = (prefix = '', size = 4, randomSet = alphabet) => {
     const nnid = customAlphabet(randomSet, size);
